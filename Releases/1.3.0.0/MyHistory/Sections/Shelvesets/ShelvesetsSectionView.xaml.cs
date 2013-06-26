2018d12f63abb6ca553c28437a5a296e9da63eaa
mikeFourie_cp <SND\mikeFourie_cp>
2013-06-26 10:09:37 +0100

    Branched from $/MyHistory/Main

---
+++ Releases/1.3.0.0/MyHistory/Sections/Shelvesets/ShelvesetsSectionView.xaml.cs
@@ -0,0 +1,75 @@
+﻿// <copyright file="ShelvesetsSectionView.xaml.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
+namespace Microsoft.ALMRangers.Samples.MyHistory
+{
+    using System.Windows;
+    using System.Windows.Input;
+    using Microsoft.TeamFoundation.VersionControl.Client;
+
+    /// <summary>
+    /// Shelvesets Section View
+    /// </summary>
+    public partial class ShelvesetsSectionView
+    {
+        public static readonly DependencyProperty ParentSectionProperty = DependencyProperty.Register("ParentSection", typeof(ShelvesetsSection), typeof(ShelvesetsSectionView));
+
+        public ShelvesetsSectionView()
+        {
+            this.InitializeComponent();
+        }
+
+        public ShelvesetsSection ParentSection
+        {
+            get
+            {
+                return (ShelvesetsSection)GetValue(ParentSectionProperty);
+            }
+
+            set
+            {
+                SetValue(ParentSectionProperty, value);
+            }
+        }
+
+        public int SelectedIndex
+        {
+            get
+            {
+                return ListViewShelvesets.SelectedIndex;
+            }
+
+            set
+            {
+                ListViewShelvesets.SelectedIndex = value;
+                ListViewShelvesets.ScrollIntoView(ListViewShelvesets.SelectedItem);
+            }
+        }
+
+        private void ListViewShelvesets_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && ListViewShelvesets.SelectedItems.Count == 1)
+            {
+                this.ViewShelvesetDetails();
+            }
+        }
+
+        private void ListViewShelvesets_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && ListViewShelvesets.SelectedItems.Count == 1)
+            {
+                this.ViewShelvesetDetails();
+            }
+        }
+
+        private void ViewShelvesetDetails()
+        {
+            if (ListViewShelvesets.SelectedItems.Count == 1)
+            {
+                Shelveset shelveset = ListViewShelvesets.SelectedItems[0] as Shelveset;
+                if (shelveset != null)
+                {
+                    this.ParentSection.ViewShelvesetDetails(shelveset);
+                }
+            }
+        }
+    }
+}
---
+++ Releases/1.3.0.0/MyHistory/Sections/WorkItems/WorkItemsSectionView.xaml.cs
@@ -0,0 +1,75 @@
+﻿// <copyright file="WorkItemsSectionView.xaml.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
+namespace Microsoft.ALMRangers.Samples.MyHistory
+{
+    using System.Windows;
+    using System.Windows.Input;
+    using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+    /// <summary>
+    /// WorkItemsSectionView
+    /// </summary>
+    public partial class WorkItemsSectionView
+    {
+        public static readonly DependencyProperty ParentSectionProperty = DependencyProperty.Register("ParentSection", typeof(WorkItemsSection), typeof(WorkItemsSectionView));
+
+        public WorkItemsSectionView()
+        {
+            this.InitializeComponent();
+        }
+
+        public int SelectedIndex
+        {
+            get
+            {
+                return workItemList.SelectedIndex;
+            }
+
+            set
+            {
+                workItemList.SelectedIndex = value;
+                workItemList.ScrollIntoView(workItemList.SelectedItem);
+            }
+        }
+
+        public WorkItemsSection ParentSection
+        {
+            get
+            {
+                return (WorkItemsSection)GetValue(ParentSectionProperty);
+            }
+
+            set
+            {
+                SetValue(ParentSectionProperty, value);
+            }
+        }
+
+        private void WorkItemList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && this.workItemList.SelectedItems.Count == 1)
+            {
+                this.ViewWorkItemDetails();
+            }
+        }
+
+        private void WorkItemList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && this.workItemList.SelectedItems.Count == 1)
+            {
+                this.ViewWorkItemDetails();
+            }
+        }
+
+        private void ViewWorkItemDetails()
+        {
+            if (this.workItemList.SelectedItems.Count == 1)
+            {
+                WorkItem wi = this.workItemList.SelectedItems[0] as WorkItem;
+                if (wi != null)
+                {
+                    this.ParentSection.ViewWorkItemDetails(wi.Id);
+                }
+            }
+        }
+    }
+}
