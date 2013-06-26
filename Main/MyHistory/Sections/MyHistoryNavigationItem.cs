a61792ca7a70493439f04ef435a97fbcc51fd5ac
mikeFourie_cp <SND\mikeFourie_cp>
2013-06-26 10:08:52 +0100

    1.3 - MyHistory changed from NavigationLink to NavigationItem

--- Main/MyHistory/Sections/MyHistoryNavigationItem.cs
+++ Main/MyHistory/Sections/MyHistoryNavigationItem.cs
@@ -1,22 +1,25 @@
-﻿// <copyright file="MyHistoryNavigationLink.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
+﻿// <copyright file="MyHistoryNavigationItem.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
 namespace Microsoft.ALMRangers.Samples.MyHistory
 {
     using System;
     using System.ComponentModel.Composition;
+    using System.Drawing;
     using Microsoft.TeamFoundation.Controls;
     using Microsoft.VisualStudio.Shell;
 
-    [TeamExplorerNavigationLink(MyHistoryNavigationLink.LinkId, TeamExplorerNavigationItemIds.MyWork, 200)]
-    public class MyHistoryNavigationLink : TeamExplorerBaseNavigationLink
+    [TeamExplorerNavigationItem(MyHistoryNavigationItem.LinkId, 200)]
+    public class MyHistoryNavigationItem : TeamExplorerBaseNavigationItem
     {
         public const string LinkId = "e49a882b-1677-46a9-93b4-db290943bbcd";
 
         [ImportingConstructor]
-        public MyHistoryNavigationLink([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider) : base(serviceProvider)
+        public MyHistoryNavigationItem([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider) : base(serviceProvider)
         {
             this.Text = "My History";
             this.IsVisible = true;
-            this.IsEnabled = true;
+
+            Image bmp = Image.FromStream(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("Microsoft.ALMRangers.Samples.MyHistory.Resources.MyHistory.png"));
+            this.Image = bmp;
         }
 
         public override void Execute()
@@ -38,7 +41,6 @@ namespace Microsoft.ALMRangers.Samples.MyHistory
         public override void Invalidate()
         {
             base.Invalidate();
-            this.IsEnabled = true;
             this.IsVisible = true;
         }
     }
