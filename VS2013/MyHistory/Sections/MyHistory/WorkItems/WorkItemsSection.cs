9d3e463faceb5fe32d71e49e666997897b5baee8
mikeFourie_cp <SND\mikeFourie_cp>
2013-07-20 14:22:40 +0100

    Branched from $/MyHistory/Main

---
+++ VS2013/MyHistory/Sections/MyHistory/WorkItems/WorkItemsSection.cs
@@ -0,0 +1,169 @@
+﻿// <copyright file="WorkItemsSection.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
+namespace Microsoft.ALMRangers.Samples.MyHistory
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+    using EnvDTE;
+    using Microsoft.TeamFoundation.Client;
+    using Microsoft.TeamFoundation.Controls;
+    using Microsoft.TeamFoundation.WorkItemTracking.Client;
+    using Microsoft.VisualStudio.TeamFoundation.WorkItemTracking;
+
+    [TeamExplorerSection(WorkItemsSection.SectionId, MyHistoryPage.PageId, 10)]
+    public class WorkItemsSection : TeamExplorerBaseSection
+    {
+        public const string SectionId = "03597647-2F81-42B0-AA45-0D8E6DD54679";
+        private ObservableCollection<WorkItem> workItems = new ObservableCollection<WorkItem>();
+
+        public WorkItemsSection()
+        {
+            this.Title = "Work Items";
+            this.IsVisible = true;
+            this.IsExpanded = true;
+            this.IsBusy = false;
+            this.SectionContent = new WorkItemsSectionView();
+            this.View.ParentSection = this;
+        }
+
+        public ObservableCollection<WorkItem> WorkItems
+        {
+            get
+            {
+                return this.workItems;
+            }
+
+            protected set
+            {
+                this.workItems = value;
+                this.RaisePropertyChanged("WorkItems");
+            }
+        }
+
+        protected WorkItemsSectionView View
+        {
+            get { return this.SectionContent as WorkItemsSectionView; }
+        }
+
+        public void ViewWorkItemDetails(int workItemId)
+        {
+            try
+            {
+                ITeamFoundationContext context = this.CurrentContext;
+                EnvDTE80.DTE2 dte2 = Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(DTE)) as EnvDTE80.DTE2;
+                if (dte2 != null)
+                {
+                    DocumentService witDocumentService = (DocumentService)dte2.DTE.GetObject("Microsoft.VisualStudio.TeamFoundation.WorkItemTracking.DocumentService");
+                    var widoc = witDocumentService.GetWorkItem(context.TeamProjectCollection, workItemId, this);
+                    witDocumentService.ShowWorkItem(widoc);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.ShowNotification(ex.Message, NotificationType.Error);
+            }
+        }
+
+        public async override void Initialize(object sender, SectionInitializeEventArgs e)
+        {
+            base.Initialize(sender, e);
+
+            // If the user navigated back to this page, there could be saved context information that is passed in
+            var sectionContext = e.Context as ChangesSectionContext;
+            if (sectionContext != null)
+            {
+                // Restore the context instead of refreshing
+                ChangesSectionContext context = sectionContext;
+                this.WorkItems = context.WorkItems;
+            }
+            else
+            {
+                // Kick off the refresh
+                await this.RefreshAsync();
+            }
+        }
+
+        /// <summary>
+        /// Refresh override.
+        /// </summary>
+        public async override void Refresh()
+        {
+            base.Refresh();
+            await this.RefreshAsync();
+        }
+
+        /// <summary>
+        /// Save contextual information about the current section state.
+        /// </summary>
+        public override void SaveContext(object sender, SectionSaveContextEventArgs e)
+        {
+            base.SaveContext(sender, e);
+
+            // Save our current so when the user navigates back to the page the content is restored rather than requeried
+            ChangesSectionContext context = new ChangesSectionContext { WorkItems = this.WorkItems };
+            e.Context = context;
+        }
+
+        /// <summary>
+        /// ContextChanged override.
+        /// </summary>
+        protected override async void ContextChanged(object sender, TeamFoundation.Client.ContextChangedEventArgs e)
+        {
+            base.ContextChanged(sender, e);
+
+            // If the team project collection or team project changed, refresh the data for this section
+            if (e.TeamProjectCollectionChanged || e.TeamProjectChanged)
+            {
+                await this.RefreshAsync();
+            }
+        }
+
+        private async Task RefreshAsync()
+        {
+            try
+            {
+                // Set our busy flag and clear the previous data
+                this.IsBusy = true;
+                this.WorkItems.Clear();
+
+                ObservableCollection<WorkItem> lworkItems = new ObservableCollection<WorkItem>();
+
+                // Make the server call asynchronously to avoid blocking the UI
+                await Task.Run(() =>
+                {
+                    ITeamFoundationContext context = this.CurrentContext;
+                    if (context != null && context.HasCollection && context.HasTeamProject)
+                    {
+                        WorkItemStore wis = context.TeamProjectCollection.GetService<WorkItemStore>();
+                        if (wis != null)
+                        {
+                            WorkItemCollection wic = wis.Query("SELECT [System.Id], [System.Title], [System.State] FROM WorkItems WHERE [System.WorkItemType] <> ''  AND  [System.State] <> ''  AND  [System.AssignedTo] EVER @Me ORDER BY [System.ChangedDate] desc");
+                            int i = 0;
+                            foreach (WorkItem wi in wic)
+                            {
+                                lworkItems.Add(wi);
+                                i++;
+                                if (i >= 10)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                });
+
+                // Now back on the UI thread, update the bound collection and section title
+                this.WorkItems = lworkItems;
+            }
+            catch (Exception ex)
+            {
+                this.ShowNotification(ex.Message, NotificationType.Error);
+            }
+            finally
+            {
+                // Always clear our busy flag when done
+                this.IsBusy = false;
+            }
+        }
+    }
+}
---
+++ VS2013/MyHistory/WorkItemsLink.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="WorkItemsLink.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
+namespace Microsoft.ALMRangers.Samples.MyHistory
+{
+    using System;
+    using System.ComponentModel.Composition;
+    using Microsoft.TeamFoundation.Controls;
+    using Microsoft.VisualStudio.Shell;
+
+    [TeamExplorerNavigationLink(WorkItemsLink.LinkId, MyHistoryNavigationItem.LinkId, 10)]
+    public class WorkItemsLink : TeamExplorerBaseNavigationLink
+    {
+        public const string LinkId = "13BC3C19-ED66-4423-BC87-D28B9471A20E";
+
+        [ImportingConstructor]
+        public WorkItemsLink([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+            this.Text = "Work Items";
+            this.IsVisible = true;
+            this.IsEnabled = true;
+        }
+
+        public override void Execute()
+        {
+            try
+            {
+                ITeamExplorer teamExplorer = GetService<ITeamExplorer>();
+                if (teamExplorer != null)
+                {
+                    teamExplorer.NavigateToPage(new Guid(WorkItemsPage.PageId), null);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.ShowNotification(ex.Message, NotificationType.Error);
+            }
+        }
+
+        public override void Invalidate()
+        {
+            base.Invalidate();
+            this.IsEnabled = true;
+            this.IsVisible = true;
+        }
+    }
+}
