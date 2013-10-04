cebe7c6573cd4690b204737bcc5380c8ae82bd23
mikeFourie_cp <SND\mikeFourie_cp>
2013-10-04 13:15:34 +0100

    Branched from $/MyHistory/Main

---
+++ Releases/1.11.6.0/MyHistory/Internals/ChangesSectionContext.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ChangesSectionContext.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
+namespace Microsoft.ALMRangers.Samples.MyHistory
+{
+    using System.Collections.ObjectModel;
+    using Microsoft.TeamFoundation.VersionControl.Client;
+    using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+    /// <summary>
+    /// Class to preserve the contextual information for this section.
+    /// </summary>
+    internal class ChangesSectionContext
+    {
+        public ObservableCollection<Changeset> Changesets { get; set; }
+
+        public ObservableCollection<WorkItem> WorkItems { get; set; }
+
+        public ObservableCollection<Shelveset> Shelvesets { get; set; }
+    }
+}
---
+++ Releases/1.11.6.0/MyHistory/Pages/ChangesetsPage.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="ChangesetsPage.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
+namespace Microsoft.ALMRangers.Samples.MyHistory
+{
+    using Microsoft.TeamFoundation.Controls;
+
+    /// <summary>
+    /// We are extending Team Explorer by adding a new page and therefore use the TeamExplorerPage attribute and pass in our unique ID
+    /// </summary>
+    [TeamExplorerPage(ChangesetsPage.PageId)]
+    public class ChangesetsPage : TeamExplorerBasePage
+    {
+        // All Pages must have a unique ID. Use the Tools - Create GUID menu in Visual Studio to create your own GUID
+        public const string PageId = "234C4E71-D513-40CC-8CF0-AAB67DAD2C1E";
+
+        public ChangesetsPage()
+        {
+            // Set the page title
+            this.Title = "My History - Changesets";
+        }
+    }
+}
