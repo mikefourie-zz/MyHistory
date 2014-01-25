9989c17b9d8ddec4acf9b61b29ee450ec0f75efd
mikeFourie_cp <SND\mikeFourie_cp>
2014-01-25 14:28:48 +0000

    RI - deeper colleague search

--- Main/MyHistory/Properties/VersionInfo.cs
+++ Main/MyHistory/Properties/VersionInfo.cs
@@ -1,5 +1,5 @@
 // <copyright file="VersionInfo.cs" company="Microsoft Corporation">Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.</copyright>
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.11.0.0")]
-[assembly: AssemblyFileVersion("2.11.0.0")]
+[assembly: AssemblyVersion("2.11.0.1")]
+[assembly: AssemblyFileVersion("2.11.0.1")]
